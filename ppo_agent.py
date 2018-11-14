47a8b0bd9dfb5305d7661f076b8a89214e6883bb
pablobd <pablo.barbero.domeno@gmail.com>
2018-11-14 09:13:21 +0100

    change name of agent file

---
+++ ppo_agent.py
@@ -0,0 +1,54 @@
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+from model import QNetwork
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+BUFFER_SIZE = int(1e7)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-4               # learning rate 
+UPDATE_EVERY = 4        # how often to update the network
+
+
+
+
+# return sum of log-prob divided by T
+# same thing as -policy_loss
+def surrogate(policy, old_probs, states, actions, rewards,
+              discount = 0.995, beta=0.01):
+
+    discount = discount**np.arange(len(rewards))
+    rewards = np.asarray(rewards)*discount[:,np.newaxis]
+    
+    # convert rewards to future rewards
+    rewards_future = rewards[::-1].cumsum(axis=0)[::-1]
+    
+    mean = np.mean(rewards_future, axis=1)
+    std = np.std(rewards_future, axis=1) + 1.0e-10
+
+    rewards_normalized = (rewards_future - mean[:,np.newaxis])/std[:,np.newaxis]
+    
+    # convert everything into pytorch tensors and move to gpu if available
+    actions = torch.tensor(actions, dtype=torch.int8, device=device)
+    old_probs = torch.tensor(old_probs, dtype=torch.float, device=device)
+    rewards = torch.tensor(rewards_normalized, dtype=torch.float, device=device)
+
+    # convert states to policy (or probability)
+    new_probs = states_to_prob(policy, states)
+    new_probs = torch.where(actions == RIGHT, new_probs, 1.0-new_probs)
+
+    ratio = new_probs/old_probs
+
+    # include a regularization term
+    # this steers new_policy towards 0.5
+    # add in 1.e-10 to avoid log(0) which gives nan
+    entropy = -(new_probs*torch.log(old_probs+1.e-10)+ \
+        (1.0-new_probs)*torch.log(1.0-old_probs+1.e-10))
+
+    return torch.mean(ratio*rewards + beta*entropy)
