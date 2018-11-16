155c9b865ce73f30625c7d1d5535e88a93a21591
pablobd <pablo.barbero.domeno@gmail.com>
2018-11-16 07:37:05 +0100

    add ppo agent file where the agent class is defined

--- ppo_agent.py
+++ ppo_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+#from model import ppoNetwork
 
 import torch
 import torch.nn.functional as F
@@ -17,38 +17,64 @@ UPDATE_EVERY = 4        # how often to update the network
 
 
 
+class ppoAgent():
+    """Interacts with and learns from the environment."""
 
-# return sum of log-prob divided by T
-# same thing as -policy_loss
-def surrogate(policy, old_probs, states, actions, rewards,
-              discount = 0.995, beta=0.01):
+    def __init__(self, state_size, action_size, hidden_layers = [64, 64], seed = 0):#drop_p = 0.8, 
+        """Initialize an Agent object.
+        
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
 
-    discount = discount**np.arange(len(rewards))
-    rewards = np.asarray(rewards)*discount[:,np.newaxis]
+        # Q-Network
+        self.qnetwork_local = ppoNetwork(state_size, action_size, hidden_layers, seed).to(device) #drop_p, 
+        self.qnetwork_target = ppoNetwork(state_size, action_size, hidden_layers, seed).to(device) #drop_p, 
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0  
+
     
-    # convert rewards to future rewards
-    rewards_future = rewards[::-1].cumsum(axis=0)[::-1]
+    def surrogate(policy, old_probs, states, actions, rewards,
+                  discount = 0.995, beta=0.01):        
+        """ returns sum of log-prob divided by T same thing as -policy_loss """ 
+        
+        discount = discount**np.arange(len(rewards))
+        rewards = np.asarray(rewards)*discount[:,np.newaxis]
     
-    mean = np.mean(rewards_future, axis=1)
-    std = np.std(rewards_future, axis=1) + 1.0e-10
-
-    rewards_normalized = (rewards_future - mean[:,np.newaxis])/std[:,np.newaxis]
+        # convert rewards to future rewards
+        rewards_future = rewards[::-1].cumsum(axis=0)[::-1]
     
-    # convert everything into pytorch tensors and move to gpu if available
-    actions = torch.tensor(actions, dtype=torch.int8, device=device)
-    old_probs = torch.tensor(old_probs, dtype=torch.float, device=device)
-    rewards = torch.tensor(rewards_normalized, dtype=torch.float, device=device)
+        mean = np.mean(rewards_future, axis=1)
+        std = np.std(rewards_future, axis=1) + 1.0e-10
 
-    # convert states to policy (or probability)
-    new_probs = states_to_prob(policy, states)
-    new_probs = torch.where(actions == RIGHT, new_probs, 1.0-new_probs)
+        rewards_normalized = (rewards_future - mean[:,np.newaxis])/std[:,np.newaxis]
+    
+        # convert everything into pytorch tensors and move to gpu if available
+        actions = torch.tensor(actions, dtype=torch.int8, device=device)
+        old_probs = torch.tensor(old_probs, dtype=torch.float, device=device)
+        rewards = torch.tensor(rewards_normalized, dtype=torch.float, device=device)
 
-    ratio = new_probs/old_probs
+        # convert states to policy (or probability)
+        new_probs = states_to_prob(policy, states)
+        new_probs = torch.where(actions == RIGHT, new_probs, 1.0-new_probs)
 
-    # include a regularization term
-    # this steers new_policy towards 0.5
-    # add in 1.e-10 to avoid log(0) which gives nan
-    entropy = -(new_probs*torch.log(old_probs+1.e-10)+ \
+        ratio = new_probs/old_probs
+
+        # include a regularization term
+        # this steers new_policy towards 0.5
+        # add in 1.e-10 to avoid log(0) which gives nan
+        entropy = -(new_probs*torch.log(old_probs+1.e-10)+ \
         (1.0-new_probs)*torch.log(1.0-old_probs+1.e-10))
 
-    return torch.mean(ratio*rewards + beta*entropy)
+        return torch.mean(ratio*rewards + beta*entropy)
+    
