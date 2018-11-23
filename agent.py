9915dd59ed68e437882a68619d365ee1e6f88c1b
pablobd <pablo.barbero.domeno@gmail.com>
2018-11-23 18:16:43 +0100

    import libraries and start with the classes we want to define

---
+++ agent.py
@@ -0,0 +1,20 @@
+import numpy as np
+from collections import namedtuple, deque
+import random, copy
+
+from model import Actor, Critic
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Agent:
+    " Interacts with and learns from the environment "
+    
+    
+class ReplayBuffer:
+    " Internal memory of the agent "
+    
+    
+    
+
