2f923dd1241c5c9d65b7f2c4fb43dd78f4bce0be
pablobd <pablo.barbero.domeno@gmail.com>
2018-12-01 12:45:40 +0100

    finalize ReplayBuffer class for the agent memory

--- agent.py
+++ agent.py
@@ -8,6 +8,15 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 128        # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_CRITIC = 3e-4        # learning rate of the critic
+WEIGHT_DECAY = 0.0001   # L2 weight decay
+
+
 class Agent:
     " Interacts with and learns from the environment "
     
@@ -31,8 +40,17 @@ class Agent:
         self.local_critic = Critic(action_size, state_size, random_seed, hidden_layers)
         self.target_critic = Critic(action_size, state_size, random_seed, hidden_layers)
         
-        self.experiencies = ReplayBuffer()
+        self.experiencies = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE)
         
+    def step(self)
+        
+    def act(self)
+    
+    def learn(self)
+    
+    def learn_soft(self)
+    
+    
         
     
 class ReplayBuffer:
@@ -46,14 +64,35 @@ class ReplayBuffer:
             batch_size (int): size of each training batch
         """
     
-        self.experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state', 'done'])
+        self.experience = namedtuple('Experience', ['state', 'action', 'reward', 'next_state', 'done']).float().to(device)
         self.memory = deque()
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
         
     def add(self, state, action, reward, next_state, done):
         " Add a new experience to memory "
         
         self.experience = (state, action, reward, next_state, done)
         self.memory.append(experience)
+        
+    def sample(self):
+        " Randomly sample a batch of experiences from the memory "
+        
+        batch = random.sample(self.memory, self.batch_size)
+        
+        states = torch.from_numpy([exp.state for exp in batch if exp is not None]).float().to(device)
+        actions = torch.from_numpy([exp.action for exp in batch if exp is not None]).float().to(device)
+        rewards = torch.from_numpy([exp.reward for exp in batch if exp is not None]).float().to(device)
+        next_states = torch.from_numpy([exp.next_state for exp in batch if exp is not None]).float().to(device)
+        dones = torch.from_numpy([exp.done for exp in batch if exp is not None]).float().to(device)
+        
+        return (states, actions, rewards, next_states, dones)
+    
+    def __len__(self):
+        " Return the current size of internal memory. Overwrites the inherited function len. "
+        
+        return len(self.memory)
+        
     
     
 
