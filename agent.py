4f3646ff033b3de4f5b2f5908bc8aa8c6eebcabd
pablobd <pablo.barbero.domeno@gmail.com>
2018-12-06 16:13:01 +0100

    add Ornsetein-Uhlenbeck noise to the action space of Agent in train mode

--- agent.py
+++ agent.py
@@ -76,10 +76,10 @@ class Agent:
         with torch.no_grad():
             action = self.local_actor(state).cpu().data.numpy()
             
-        """self.local_actor.train() # set nework on train mode
+        self.local_actor.train() # set nework on train mode
         if add_noise:
             action += self.noise.sample()
-        return np.clip(action, -1, 1)"""
+        return np.clip(action, -1, 1)
         
         return action
     
@@ -138,8 +138,31 @@ class Agent:
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
-            
+           
         
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
+    
+    
     
 class ReplayBuffer:
     " Internal memory of the agent "
